e7a5adae9793883d9c290402da96739341a86ec0
Biser <korcheto@abv.bg>
2022-06-09 08:20:36 +0300

    Add files via upload

---
+++ DOM - Exercises/01.Edit Element/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let inputElement = document.getElementById('text');
+    inputElement.style.display = 'inline';
+    let secElement = document.getElementById('more');
+    secElement.style.display = 'none';
+}
