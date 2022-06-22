de8c9c54b85a491c1ff7e6896e6abee4e444bdd2
Biser <korcheto@abv.bg>
2022-06-22 19:29:44 +0300

    Add files via upload

---
+++ Unit-testing/addOrSubtract.test.js
@@ -0,0 +1,38 @@
+const createCalculator = require('./addOrSubtract.js');
+const { expect } = require('chai');
+
+describe('createCalculator', () => {
+    it ('check whether it returns properties are functions', () => {
+        expect(typeof createCalculator().add).to.equal('function');
+        expect(typeof createCalculator().subtract).to.equal('function');
+        expect(typeof createCalculator().get).to.equal('function');
+    });
+
+
+    it ('check if add and subtract calsulate properly', () => {
+        let calc = createCalculator();
+        calc.add('5');
+        expect(calc.get()).to.equal(5);
+    });
+
+    it ('check if add and subtract get only numbers', () => {
+        let calc = createCalculator();
+        calc.add('g');
+        expect(calc.get()).to.be.NaN;
+    });
+
+    it ('value cant be changed', () => {
+        let calc = createCalculator();
+        expect(calc.value += 4).to.be.NaN;
+    });
+
+    it ('value cant be accesed', () => {
+        let calc = createCalculator();
+        expect(calc.value).to.be.undefined;
+    });
+
+    it ('returns an object', () => {
+        let calc = createCalculator();
+        expect(calc).to.be.a('object');
+    });
+})
